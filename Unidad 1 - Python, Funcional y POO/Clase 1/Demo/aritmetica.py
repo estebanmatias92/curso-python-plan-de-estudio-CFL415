a8b752661b5fb8d0d07e0ca04720fd1004f18edf
estebanmatias92 <estebanmatias92@gmail.com>
2023-03-20 23:56:27 -0300

    Añadidos mas ejemplos a la clase 1

---
+++ Unidad 1 - Python, Funcional y POO/Clase 1/Demo/aritmetica.py
@@ -0,0 +1,17 @@
+
+# Inicializando varibles con valores
+mi_var1 = 100
+mi_var2 = 5
+
+# Usando esas variables para operaciones matematicas y guardando los resultados en otras variables
+suma = mi_var1 + mi_var2
+resta = mi_var1 - mi_var2
+multiplicacion = mi_var1 * mi_var2
+division = mi_var1 / mi_var2
+
+
+# Usando los resultados para mostrarlos por pantalla
+print("\nEl resultado de la suma es: ", suma)
+print("\nEl resultado de la suma es: ", resta)
+print("\nEl resultado de la suma es: ", multiplicacion)
+print("\nEl resultado de la suma es: ", division)
--- Unidad 1 - Python, Funcional y POO/Clase 1/Demo/hola_mundo.py
+++ Unidad 1 - Python, Funcional y POO/Clase 1/Demo/hola_mundo.py
@@ -1,13 +1,5 @@
-# Ingresar texto con mi nombre
+# Ingresar por tecladomi nombre
 nombre_apellido = input("Ingrese su nombre: ")
-# Ingresar si soy estudiante
-soy_estudiante = bool(int(input("Es estudiante: ")))
-# Ingrese edad en enteros
-mi_edad = int(input("Ingrese su edad: "))
-# Ingresar salario
-mi_salario = float(input("Ingrese su salario: "))
 
-print("Nombre: ", nombre_apellido)
-print("Estudio?: ", soy_estudiante)
-print("Edad: ", mi_edad)
-print("Salario: ", mi_salario)
+# Mostrando por pantalla mensaje, usando la variable
+print("Hola ", nombre_apellido, ", un gusto!")
