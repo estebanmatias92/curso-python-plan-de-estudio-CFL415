339fde43f99154115ace31942218ebf2a83ff17d
estebanmatias92 <estebanmatias92@gmail.com>
2023-03-21 18:09:24 -0300

    Añadidos ejemplos comparacion clase 1

---
+++ Unidad 1 - Python, Funcional y POO/Clase 1/Demo/operadores_comparacion.py
@@ -0,0 +1,14 @@
+# Pedir un numero al ususario
+num1 = input("Ingrese el primer numero a comparar: ")
+num2 = input("Ingrese el segundo numero a comparar: ")
+
+
+print("")
+
+# Las comparaciones siempre devuelvo True o False, nada mas
+print("Comparando si num1 es igual a num2: ", num1 == num2)                     # Mostrando si ambos numeros son iguales
+print("Comparando si num1 es distinto a num2: ", num1 != num2)                  # Mostrando si ambos numeros son distinto   
+print("Comparando si num1 es mayor o iguala num2: ", num1 >= num2)              # Mostrando si el primer numero es mayor o igual al segundo
+print("Comparando si num1 es mayor a num2: ", num1 > num2)                      # Mostrando si el primer numero es mayor al segundo
+print("Comparando si num1 es menor o igual a num2: ", num1 <= num2)             # Mostrando si el primer numero es menor o igual al segundo
+print("Comparando si num1 es menor a num2: ", num1 < num2)                      # Mostrando si el primer numero es menor al segundo
