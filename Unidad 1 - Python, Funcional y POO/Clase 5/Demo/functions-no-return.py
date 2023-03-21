be22747640950047b405a7f7ca0ed210ff13b269
estebanmatias92 <estebanmatias92@gmail.com>
2023-03-21 01:00:51 -0300

    Refactorizado los ejemplos de la clase 5

---
+++ Unidad 1 - Python, Funcional y POO/Clase 5/Demo/functions-no-return.py
@@ -0,0 +1,21 @@
+
+print("\nFuncion que solo saluda por pantalla: ")
+# Funcion sin parametro ni retorno
+def saludar():                      # Definiendo la funcion, No tiene parametros
+    print("Salundo a todos!\n")     # No retorna nada, solo muestra por pantalla
+
+saludar()                           # Usando la funcion
+
+
+
+
+
+print("\nFuncion que comprueba si un numero es par o impar: ")
+# Función sin return
+def comprobar_paridad(numerito):    # Definicnedo la funcion, tiene un parametro
+    if numerito %2 == 0:
+        print(f"El numero {numerito} es par!\n")
+    else:    
+        print(f"El numero {numerito} es impar!\n")
+
+comprobar_paridad(5)                # Usando la funcion, pasando el valor 5 como argumento
--- Unidad 1 - Python, Funcional y POO/Clase 5/Demo/functions.py
+++ Unidad 1 - Python, Funcional y POO/Clase 5/Demo/functions.py
@@ -1,15 +1,4 @@
 
-
-print("\nFuncion que solo saluda por pantalla: ")
-# Funcion sin parametro ni retorno
-def saludar():                      # Definiendo la funcion, No tiene parametros
-    print("Salundo a todos!\n")     # No retorna nada, solo muestra por pantalla
-
-saludar()                           # Usando la funcion, no necesita pasar argumentos
-
-
-
-
 print("\nFuncion de suma con 3 parametros: ")
 # Funcion que tiene 3 parametros, osea, que toma 3 argumentos y devuelve su suma
 def sumar(num1, num2, num3):
@@ -17,19 +6,6 @@ def sumar(num1, num2, num3):
 
 print("Resultado: ", sumar(3, 3, 3))                      # Sumando numeros
 print("Resultado: ", sumar("Soy ", "un ", "String!"))     # Concatenando strings                 # Sumando numeros
-
-
-
-
-print("\nFuncion que comprueba si un numero es par o impar: ")
-# Función sin return
-def comprobar_paridad(numerito):    # Definicnedo la funcion, tiene un parametro
-    if numerito %2 == 0:
-        print(f"El numero {numerito} es par!\n")
-    else:    
-        print(f"El numero {numerito} es impar!\n")
-
-comprobar_paridad(5)                # Usando la funcion, pasando el valor 5 como argumento
 
 
 
